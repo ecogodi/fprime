045bea3ed6d8d5ea897e4b974872a1d0ac8dc379
elia <elia.cogodi@3logic.it>
2015-01-05 12:14:15 +0100

    coverage script: create output test/cov directory if necessary

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,3 +1,7 @@
+if [ ! -d test/cov ]; then
+    mkdir test/cov
+fi
+
 ./node_modules/.bin/jscoverage lib test/cov/lib
 ./node_modules/.bin/jscoverage fprime.js test/cov/fprime.js
 env TESTMODE=COVERAGE ./node_modules/.bin/mocha -R html-cov > test/cov/coverage_report.html
